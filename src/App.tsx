b132ef6d3a6144517982074768b689f135c30af8
Nikita Chaika <iasastudent2021@gmail.com>
2023-10-17 21:56:01 +0300

    feat: added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import Layout from "@components/Layout";
 import { routes } from "@core/routes";
 
 export const App: FC = () => {
   return (
     <Routes>
-      {Object.values(routes).map((route) => (
-        <Route
-          key={`route-${route.path}`}
-          path={route.path}
-          element={<route.Element />}
-        />
-      ))}
+      <Route path="/" element={<Layout />}>
+        {Object.values(routes).map((route) => (
+          <Route
+            key={`route-${route.path}`}
+            path={route.path}
+            element={<route.Element />}
+          />
+        ))}
+      </Route>
     </Routes>
   );
 };
