0b0ad57af7b0151b06e474bb9438ee7f1e12ab47
Nikita Chaika <iasastudent2021@gmail.com>
2023-10-23 01:09:30 +0300

    feat(home): added home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { ROUTE_PATHS } from "@constants/routePaths";
 
 import "./Home.scss";
 
 export const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <h1 className="home__title">
+        Welcome to <span className="home__title_green">Freshnesecom</span> !
+      </h1>
+      <Link className="home__link" to={ROUTE_PATHS.PRODUCTS}>
+        Go to the products
+      </Link>
+    </section>
+  );
 };
