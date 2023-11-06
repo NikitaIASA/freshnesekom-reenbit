3346e2530ef6ec60c714452bbc3ec3b02c24a3df
Nikita Chaika <iasastudent2021@gmail.com>
2023-11-07 01:11:21 +0200

    feat(Sidebar): added logic for price block

--- src/components/Sidebar/PriceBlock/PriceBlock.tsx
+++ src/components/Sidebar/PriceBlock/PriceBlock.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect, useCallback} from "react";
 import Slider from "react-slider";
 
 import SidebarSectionTitle from "../SidebarSectionTitle";
@@ -6,6 +6,8 @@ import { useAppDispatch } from "@hooks/useAppDispatch";
 import { useAppSelector } from "@hooks/useAppSelector";
 import { setSelectedPriceRange } from "@store/reducers/productSlice";
 import { selectFilteredPriceRange } from "@store/selectors/productSelectors";
+import { debounce } from "@helpers/debounce";
+import { PRICE_SLIDER_DELAY } from "@constants/debounceDelays";
 
 import "./Price.scss";
 
@@ -13,26 +15,43 @@ export const PriceBlock: FC = () => {
   const dispatch = useAppDispatch();
   const [minPrice, maxPrice] = useAppSelector(selectFilteredPriceRange);
   const [values, setValues] = useState<number[]>([minPrice, maxPrice]);
+  const [inputValues, setInputValues] = useState<number[]>([
+    minPrice,
+    maxPrice,
+  ]);
+  const [error, setError] = useState<boolean | null>(null);
+
+  const debounceSetValue = useCallback(debounce((newValues: number[]) => {
+    dispatch(setSelectedPriceRange(newValues));
+  }, PRICE_SLIDER_DELAY), []);
 
   useEffect(() => {
+    setInputValues([minPrice, maxPrice]);
     setValues([minPrice, maxPrice]);
-    dispatch(setSelectedPriceRange([minPrice, maxPrice]));
-  }, [minPrice, maxPrice, dispatch]);
+    debounceSetValue([minPrice, maxPrice]);
+  }, [minPrice, maxPrice, debounceSetValue]);
 
-  const handleSliderChange = (newValues: number | number[]) => {
-    if (typeof newValues === "number") {
-      return;
+  useEffect(() => {
+    const [minInputValue, maxInputValue] = inputValues;
+    if (minInputValue < minPrice || maxInputValue > maxPrice || minInputValue > maxInputValue) {
+      setError(true);
+    } else {
+      setError(null);
+      setValues(inputValues);
+      debounceSetValue(inputValues);
     }
-    setValues(newValues);
-    dispatch(setSelectedPriceRange(newValues));
-  };
+  }, [inputValues, minPrice, maxPrice, debounceSetValue]);
 
   const handleInputChange = (index: number, value: string) => {
-    const newValues = [...values];
-    const intValue = parseFloat(value) || 0;
-    newValues[index] = Math.max(minPrice, Math.min(maxPrice, intValue)); 
+    const newInputValues = [...inputValues];
+    newInputValues[index] = parseFloat(value);
+    setInputValues(newInputValues);
+  };
+
+  const handleSliderChange = (newValues: number[]) => {
     setValues(newValues);
-    dispatch(setSelectedPriceRange(newValues));
+    setInputValues(newValues);
+    debounceSetValue(newValues);
   };
 
   return (
@@ -49,29 +68,37 @@ export const PriceBlock: FC = () => {
         pearling
       />
       <div className="price-block__inputs">
-        <label className="price-block__label">
+        <label
+          className={`price-block__label ${
+            error ? "price-block__label--error" : ""
+          }`}
+        >
           Min
           <input
             type="number"
-            value={values[0]}
+            value={inputValues[0]}
             onChange={(e) => handleInputChange(0, e.target.value)}
-            className="price-block__input"
+            className={`price-block__input ${
+              error ? "price-block__input--error" : ""
+            }`}
             placeholder={`${minPrice}`}
-            min={minPrice}
-            max={values[1]} 
           />
         </label>
         <span className="price-block__input-separator">-</span>
-        <label className="price-block__label">
+        <label
+          className={`price-block__label ${
+            error ? "price-block__label--error" : ""
+          }`}
+        >
           Max
           <input
             type="number"
-            value={values[1]}
+            value={inputValues[1]}
             onChange={(e) => handleInputChange(1, e.target.value)}
-            className="price-block__input"
+            className={`price-block__input ${
+              error ? "price-block__input--error" : ""
+            }`}
             placeholder={`${maxPrice}`}
-            min={values[0]} 
-            max={maxPrice} 
           />
         </label>
       </div>
