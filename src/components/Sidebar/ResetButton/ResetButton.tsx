89b5770943573f450894a837b2853eddea961498
Nikita Chaika <iasastudent2021@gmail.com>
2023-11-13 17:37:33 +0200

    feat(Sidebar): added scroll for various parts of the sidebar

--- src/components/Sidebar/ResetButton/ResetButton.tsx
+++ src/components/Sidebar/ResetButton/ResetButton.tsx
@@ -10,10 +10,26 @@ export const ResetButton: FC = () => {
 
   const handleReset = () => {
     dispatch(resetFilter());
-    window.scrollTo({
+
+   window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+
+    // Scroll for various parts of the sidebar
+    const sidebarElement = document.querySelector(".products__sidebar-container");
+    const categoriesList = document.querySelector(".categories__list");
+    const brandsList = document.querySelector(".brands__list");
+
+    if (sidebarElement) {
+      sidebarElement.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    if (categoriesList) {
+      categoriesList.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    if (brandsList) {
+      brandsList.scrollTo({ top: 0, behavior: "smooth" });
+    }
   };
 
   return (
