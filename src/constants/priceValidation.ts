f096975757074f5b5f58c158c80d8a6b7e30ab93
Nikita Chaika <iasastudent2021@gmail.com>
2023-11-28 12:36:36 +0200

    fix: removed commented code and magic numbers

--- src/constants/priceValidation.ts
+++ src/constants/priceValidation.ts
@@ -2,3 +2,6 @@ export const DISALLOWED_PRICE_INPUT_SYMBOLS = ['e', '-', '+', '.', ','];
 export const MIN_QUANTITY = 0;
 export const STEP_QUANTITY = 1;
 export const INITIAL_QUANTITY = 0; 
+export const PERCENTAGE_SCALE = 100;
+export const DECIMAL_BASE = 10;
+export const HUNDRED_PERCENT = 100;
