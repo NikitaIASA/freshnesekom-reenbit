5e4064ded36195ae9cac42fc66ce7d6b72221f6b
Nikita Chaika <iasastudent2021@gmail.com>
2023-12-14 01:51:32 +0200

    feat(Cart): added block for empty cart

--- src/components/Checkout/Cart/CartList/CartList.tsx
+++ src/components/Checkout/Cart/CartList/CartList.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
+import { ROUTE_PATHS } from "@constants/routePaths";
+import CustomButton from "@components/UI/CustomButton";
 import { useAppSelector } from "@hooks/useAppSelector";
 import { selectCartItems } from "@store/selectors/cartSelectors";
 import { CartItem } from "../CartItem/CartItem";
@@ -8,6 +11,22 @@ import "./CartList.scss";
 
 export const CartList: FC = () => {
   const items = useAppSelector(selectCartItems);
+
+  if (!items.length) {
+    return (
+      <div className="cart-list--empty">
+        <h2 className="cart-list__title">Your cart is empty!</h2>
+        <img
+          className="cart-list__empty-cart-image"
+          src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-7359557-6024626.png"
+          alt="emprt cart"
+        />
+        <Link to={ROUTE_PATHS.PRODUCTS}>
+          <CustomButton>Go shopping</CustomButton>
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <ul className="cart-list">
